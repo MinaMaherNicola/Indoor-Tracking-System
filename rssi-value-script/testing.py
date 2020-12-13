53d09723270068e98fcbb342ee9e2d965953c2ce
MinaMaherNicola <minamahernicola@outlook.com>
2020-12-13 20:09:19 +0200

    last version of the rssi gathering script

--- rssi-value-script/testing.py
+++ rssi-value-script/testing.py
@@ -11,16 +11,18 @@ ssids = ['AP-00', 'AP-01', 'AP-02', 'AP-03', 'AP-04', 'AP-05', 'AP-06']
 
 ap_info = rssi_scanner.getAPinfo(networks=ssids, sudo=True)
 
+while (len(ap_info) != 7):
+  ap_info = rssi_scanner.getAPinfo(networks=ssids, sudo=True)
+
 data = [{}, {}, {}, {}, {}, {}, {}]
 for i in range(len(ap_info)):
   index =  int(ap_info[i]['ssid'][-1])
   data[index] = ap_info[i]
 
 with open('rssi.csv', 'a', newline='') as file:
-  fieldNames = ['ssid', 'quality', 'signal', 'mac', 'square', 'data-type']
+  fieldNames = ['ssid', 'quality', 'signal', 'mac', 'square']
 
   writer = csv.DictWriter(file, fieldnames=fieldNames)
-
-  # writer.writeheader()
+  writer.writeheader()
   for i in range(len(data)):
     writer.writerow({'ssid': data[i]['ssid'], 'quality': data[i]['quality'], 'signal': data[i]['signal'], 'mac': data[i]['mac'], 'square': squareNumber})
