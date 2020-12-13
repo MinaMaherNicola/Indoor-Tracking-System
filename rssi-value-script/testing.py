218d316a8c82460573f68bb261b44617a9d1d78c
MinaMaherNicola <minamahernicola@outlook.com>
2020-12-13 19:35:10 +0200

    Made the script with the sorting algorithms

--- rssi-value-script/testing.py
+++ rssi-value-script/testing.py
@@ -4,24 +4,23 @@ import csv
 
 interface = 'wlp2s0'
 squareNumber = input('Square Number: ')
-data_type = 'Gathering'
 
 rssi_scanner = rssi.RSSI_Scan(interface)
 
-# But your router name in here
-# If there is more than one add all the names seperated by commas
-ssids = ['AP-00', 'AP-01', 'AP-02', 'AP-03', 'AP-04', 'AP-05', 'AP-06'] # edit name of ssids for ex tp-link123
+ssids = ['AP-00', 'AP-01', 'AP-02', 'AP-03', 'AP-04', 'AP-05', 'AP-06']
 
 ap_info = rssi_scanner.getAPinfo(networks=ssids, sudo=True)
 
-with open('data.csv', 'a', newline='') as file:
+data = [{}, {}, {}, {}, {}, {}, {}]
+for i in range(len(ap_info)):
+  index =  int(ap_info[i]['ssid'][-1])
+  data[index] = ap_info[i]
+
+with open('rssi.csv', 'a', newline='') as file:
   fieldNames = ['ssid', 'quality', 'signal', 'mac', 'square', 'data-type']
 
   writer = csv.DictWriter(file, fieldnames=fieldNames)
 
-  writer.writeheader()
-  print(ap_info)
-  for i in range(len(ap_info)):
-    writer.writerow({'ssid': ap_info[i]['ssid'], 'quality': ap_info[i]['quality'], 'signal': ap_info[i]['signal'], 'mac': ap_info[i]['mac'], 'square': squareNumber, 'data-type': data_type})
-
-  
+  # writer.writeheader()
+  for i in range(len(data)):
+    writer.writerow({'ssid': data[i]['ssid'], 'quality': data[i]['quality'], 'signal': data[i]['signal'], 'mac': data[i]['mac'], 'square': squareNumber})
