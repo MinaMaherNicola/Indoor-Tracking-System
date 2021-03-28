ff4b4183bdc650b2ba6738e77150fb2c89e386c0
Mina Maher <minamahernicola@outlook.com>
2021-03-28 17:09:57 +0200

    version before changing stuff

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -4,29 +4,42 @@ import Indicator from './Indicator';
 import Map from './Map';
 import axios from 'axios';
 
-const url = 'http://127.0.0.1:5000/prototype';
-const data = [-30, -17, -42, -40, -58, -56, -36, -52];
+// const url = 'http://127.0.0.1:5000/prototype';
+const url = 'https://ips-grad-project.herokuapp.com/prototype';
+const data = [
+  [-30, -17, -42, -40, -58, -56, -36, -52],
+  [-44, -20, -36, -28, -48, -58, -42, -54],
+  [-40, -20, -32, -28, -48, -58, -44, -52]
+];
 
 const App = () => {
-  const [square, setSquare] = useState(null);
+  const [square, setSquare] = useState({ name: 's-01' });
+  const [currentSquare, setCurrentSquare] = useState(0);
+  console.log(data[currentSquare]);
 
   useEffect(() => {
     const getSquare = async () => {
       const sq = await axios({
         method: 'POST',
         url: url,
-        data: data
+        data: data[currentSquare]
       });
       setSquare(sq.data.res);
     };
-    getSquare();
-    console.log(square);
-  }, []);
-  console.log(square);
+    setInterval(() => {
+      setCurrentSquare((currentSquare) => (currentSquare + 1) % 3);
+      console.log('Current Square:');
+      console.log(currentSquare);
+      getSquare();
+      console.log('inside interval');
+    }, 5000);
+    console.log('inside useEffect');
+  }, [square]);
+
   return (
-    <div>
+    <div className="holder">
       <Navbar />
-      {/* <Indicator /> */}
+      <Indicator name={square.name} />
       <Map />
     </div>
   );
--- front/src/components/Indicator.js
+++ front/src/components/Indicator.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const Indicator = () => {
-  return <div className="indicator">&nbsp;</div>;
+const squares = [
+  { square: 's-01', top: '365px', left: '415px' },
+  { square: 's-02', top: '275px', left: '415px' },
+  { square: 's-03', top: '365px', left: '565px' },
+  { square: 's-04', top: '365px', left: '565px' }
+];
+
+const Indicator = ({ name }) => {
+  const position = squares[parseInt(name[-1]) - 1] || squares[0];
+  return (
+    <div
+      style={{ position: 'absolute', top: position.top, left: position.left }}
+      className="indicator"
+    >
+      &nbsp;
+    </div>
+  );
 };
 
 export default Indicator;
