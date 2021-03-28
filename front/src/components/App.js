c5b3353ba828ff8436b2cd3403dd13c7ebd2e62a
Mina Maher <minamahernicola@outlook.com>
2021-03-28 16:14:51 +0200

    established communication with the backend

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './Navbar';
 import Indicator from './Indicator';
 import Map from './Map';
+import axios from 'axios';
+
+const url = 'http://127.0.0.1:5000/prototype';
+const data = [-30, -17, -42, -40, -58, -56, -36, -52];
 
 const App = () => {
+  const [square, setSquare] = useState(null);
+
+  useEffect(() => {
+    const getSquare = async () => {
+      const sq = await axios({
+        method: 'POST',
+        url: url,
+        data: data
+      });
+      setSquare(sq.data.res);
+    };
+    getSquare();
+    console.log(square);
+  }, []);
+  console.log(square);
   return (
     <div>
       <Navbar />
